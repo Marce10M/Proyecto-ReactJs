a576eca966ac3d40f2a6076fa94afdff48b60761
Marce10M <89713165+Marce10M@users.noreply.github.com>
2022-09-19 11:35:31 -0600

    proyecto agregado

---
+++ src/component/pokemon-list.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+
+export default class PokemonList extends Component {
+
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            pokemonList: '',
+            filteredList: null,
+            isReady: false
+        }
+        this.handleEvent = this.handleEvent.bind(this)
+    }
+
+    handleEvent(e) {
+        const inputValue = e.target.value
+        const tempList = [...this.state.pokemonList]
+        const filteredList = tempList.filter(list => !list.name.indexOf(inputValue.toLowerCase()))
+        this.setState({
+            filteredList
+        })
+    }
+
+    componentDidMount() {
+        fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=250')
+            .then(response => {
+                return response.json();
+            })
+            .then(resultados => {
+                let pokemonList = resultados.results
+                this.setState({
+                    pokemonList,
+                    isReady: true
+                })
+            }) 
+    }
+
+    render() {
+        let pokemonList
+        if(this.state.filteredList !== null) {
+         pokemonList = this.state.filteredList
+        } else {
+         pokemonList = this.state.pokemonList
+        }
+        if (this.state.isReady) {
+            return (
+                <React.Fragment>
+                    <div className="container-fluid">
+                        <div className="row ">
+                            <div className="mx-auto pokemon">
+                                <div className="form-group pokemon__input">
+                                    <div className='text-center mt-4'>
+                                        <img src='/poke.png' height='80' className='mb-4' alt='poke api logo'/>
+                                    </div>
+                                    <input onChange={this.handleEvent} type="text" className="form-control mt-4" id="pokemon-name"
+                                    placeholder="Buscar Pokemon" />
+                                    
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container" id="pokemon-container">
+                        <div className="row d-flex justify-content-center" >
+                            {pokemonList.map((pokemon, index) => (
+                                        <button className='btn'> <img src='/ball.png' height='40' alt='poke-ball'/> {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</button>
+                            ))}
+                        </div>
+                    </div>
+                </React.Fragment>
+            )
+        } else {
+            return (
+                <div> Loading ...</div>
+                    )
+                }
+            }
+}
